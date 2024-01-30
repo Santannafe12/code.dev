469c6658949de3ff7b14e915279a792738088135
santannafe12 <santannafe12@gmail.com>
2024-01-30 15:57:44 -0300

    Carousel done

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -22,7 +22,7 @@ export default function CarouselHome({ posts }: CarouselHomeProps) {
             }}
             plugins={[
                 Autoplay({
-                    delay: 4000,
+                    delay: 6000,
                 }),
             ]}
             className="w-full"
@@ -31,18 +31,18 @@ export default function CarouselHome({ posts }: CarouselHomeProps) {
                 {posts.map((post, index) => (
                     <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                         <div className="p-1">
-                            <Card className="relative group overflow-hidden rounded-lg min-h-[500px]">
+                            <Card className="relative group overflow-hidden rounded-lg">
                                 <Image src={post.image.url} alt="placeholder" width={900} height={600} className="rounded-t-md border" priority />
                                 <div className="absolute top-0 right-0 bg-red-500 text-white text-xs px-2 py-1 rounded-bl-md font-semibold">
                                     Em alta
                                 </div>
-                                <CardHeader className="space-y-2">
+                                <CardHeader className="space-y-3">
                                     <Link href={post.slug}>
-                                        <CardTitle className="hover:underline line-clamp-2">
+                                        <CardTitle className="hover:underline line-clamp-2 min-h-12">
                                             {post.title}
                                         </CardTitle>
                                     </Link>
-                                    <CardDescription className="line-clamp-3">{post.description}</CardDescription>
+                                    <CardDescription className="line-clamp-3 min-h-16 text-pretty">{post.description}</CardDescription>
                                 </CardHeader>
                                 <CardContent className="flex flex-wrap gap-2">
                                     {post.categoriesRelationship.slice(0, 3).map((category, index) => (
